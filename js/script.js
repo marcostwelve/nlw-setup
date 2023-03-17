0d5d4c9728957958bf544154e9beb4cfa30c7c17
marcostwelve <marcostwelve>
2023-03-16 23:53:45 -0300

    Adicionado a funcionalidade de remover dados

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
 const form = document.querySelector("#form-habits");
 const nlwSetup = new NLWSetup(form);
-const button = document.querySelector("header button");
+const buttonAdd = document.querySelector("header .add");
+const buttonRemove = document.querySelector("header .remove");
+const marking = document.querySelector(".days");
 
-button.addEventListener("click", add);
+
+buttonAdd.addEventListener("click", add);
 form.addEventListener("change", save);
+buttonRemove.addEventListener("click", remove);
 
 function add() {
     const today = new Date().toLocaleDateString('pt-br').slice(0, -5);
     const dayExists = nlwSetup.dayExists(today);
+    console.log(dayExists)
     if(dayExists) {
         alert("Dia já incluso 🔴");
         return;
@@ -19,6 +24,20 @@ function add() {
 
 function save() {
     window.localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data));
+}
+
+function remove() {
+    if(window.localStorage.length != 0) {
+        window.localStorage.removeItem('NLWSetup@habits', JSON.stringify(nlwSetup.data));
+        alert("Dados removidos com sucesso 🚮");
+        marking.style.display = "none";
+        document.location.reload();
+    }
+
+    else {
+        alert("Você não tem dados salvos 💾");
+        return;
+    }
 }
 
 const data = JSON.parse(localStorage.getItem('NLWSetup@habits')) || {};
